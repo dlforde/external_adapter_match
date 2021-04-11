b2666e56e51bbd86617ce276f5a3b40bdd4aeb5e
Abdulla <abdulla.alkamil@gmail.com>
2021-04-11 16:53:08 +0100

    update app

--- test.py
+++ test.py
@@ -10,3 +10,7 @@ input = {
 }
 adapter = Adapter(input)
 print(adapter.result)
+
+
+
+# curl -d '{"id":2,"data":{"football-data":{"endpoint":"outcome","match_id":3}, "api-football": {"match_id": 202}}}' -H 'Content-Type: application/json' https://658h5tqhxi.execute-api.us-east-2.amazonaws.com/default
